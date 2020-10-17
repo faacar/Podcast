e2b2633c45f01ed98eed960642820d15cc75e9ef
Ahmet Acar <farukacar1998@gmail.com>
2020-10-17 12:57:03 +0300

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Ahmet Acar on 17.10.2020.
+//  Copyright © 2020 Ahmet Acar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
